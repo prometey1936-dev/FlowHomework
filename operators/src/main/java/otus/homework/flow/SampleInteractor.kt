0d689cb8947dfc7ba68e03fecde70af0108fec6c
Ruslan Sabirov <prometey1936@yandex.ru>
2026-01-26 00:47:50 +0300

    Реализовал функции с использование flow операторов

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 } // 1) умножаем на 5
+            .filter { it > 20 } // 2) убираем числа <= 20
+            .filter { it % 2 != 0 } // 3) убираем четные числа
+            .map { "$it won" } // 4) добавляем постфикс "won"
+            .take(3) // 5) берем 3 первых числа
     }
 
     /**
@@ -29,7 +34,15 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .flatMapConcat { number ->
+                when {
+                    number % 15 == 0 -> flowOf(number.toString(), "FizzBuzz")
+                    number % 5 == 0 -> flowOf(number.toString(), "Buzz")
+                    number % 3 == 0 -> flowOf(number.toString(), "Fizz")
+                    else -> flowOf(number.toString())
+                }
+            }
     }
 
     /**
@@ -38,7 +51,10 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { color, form ->
+                Pair(color, form)
+            }
     }
 
     /**
@@ -48,6 +64,16 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { e ->
+                if (e is IllegalArgumentException) {
+                    emit(-1)
+                } else {
+                    throw e // пробрасываем другие исключения дальше
+                }
+            }
+            .onCompletion {
+                sampleRepository.completed()
+            }
     }
 }
